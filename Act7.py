1174778e606c09a0e9f8f92d2deb676dc2c099ca
Morena Sol Cruz <79657865+MorenaCruz@users.noreply.github.com>
2022-10-26 19:08:53 -0300

    Create Act7.py

---
+++ Act7.py
@@ -0,0 +1,2 @@
+'''The convert_phone_number function checks for a U.S. phone number format: XXX-XXX-XXXX (3 digits followed by a dash, 3 more digits followed by a dash, and 4 digits), 
+and converts it to a more formal format that looks like this: (XXX) XXX-XXXX. Fill in the regular expression to complete this function.'''
